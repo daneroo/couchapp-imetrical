975924f4eaf75f0d41efd8c73fadb244967f490d
Daniel Lauzon <daniel.lauzon@gmail.com>
2011-06-13 21:09:46 -0400

    Date stuff, start converting day data into array

--- seed/seed.rb
+++ seed/seed.rb
@@ -18,26 +18,54 @@ db.documents({})['rows'].each do |r|
 end
 #puts "Pre-cached all(#{globalPlayerPoints.length}) player points"
 
-puts "now:#{Date.today} e:#{IM::EPOCH} diff:#{Date.today-IM::EPOCH}"
-IM::EPOCH.upto(Date.today) do |d| 
-  puts "++d:#{d}"
+if false 
+  puts "now:#{Date.today} e:#{IM::EPOCH} diff:#{Date.today-IM::EPOCH}"
+  IM::EPOCH.upto(Date.today) do |d| 
+    puts "++d:#{d}"
+  end
+  Date.today.downto(IM::EPOCH) do |d| 
+    puts "--d:#{d}"
+  end
+  puts "dt:now "+DateTime::now.to_s
 end
-Date.today.downto(IM::EPOCH) do |d| 
-  puts "--d:#{d}"
-end
-
-puts "dt:now "+DateTime::now.to_s
 
 baseURI = "http://192.168.5.2/iMetrical/getJSONForDay.php";
-table="watt"; # watt_minute
-((Date.today-10)..Date.today).each do |d|
+table="watt_minute"; # watt,watt_tensec,watt_minute,watt_hour
+puts sprintf("%25s %8s %8s", 'date','samples','size')
+((Date.today-1)..Date.today-1).each do |d|
+  dt=DateTime.jd(d.jd)
+  today_t = dt.to_gm_time
+  today_str=today_t.strftime"%Y-%m-%dT%H:%M:%SZ"
+  
   url = "#{baseURI}?day=#{d}&table=#{table}"
   r = Net::HTTP.get_response( URI.parse( url ) )
   json = r.body
   data = JSON.parse(json)
-  puts "#{d}  #{json.length} #{data.length}"
-  #puts JSON.pretty_generate(data)
-  #puts data.inspect
+  nudata = {
+    "stamp" => today_str,
+    "values" =>[],
+  }
+  # Speedup :
+  # convert [{w,s},{w,s}] to {w:s,w:s}
+  # (0.86400).each {|idx| fmt_and_lookup_and_remove_instead_of_parse}
+  #  check no duplicates (can't cause of mysql index on stamp)
+  #  check no unremoved key in converted hash...
+  puts sprintf("%25s %8d %8d", dt,data.length,json.length)
+  parsetime = Benchmark.realtime do
+    data.each do |pair|
+      t=DateTime.strptime(pair["stamp"]).to_gm_time
+      idx = (t-today_t).to_i 
+      #pair["t"] = t.strftime"%Y-%m-%dT%H:%M:%SZ"
+      #pair["idx"]=idx
+      nudata["values"][idx]=pair["watt"]
+      #puts pair.inspect
+      #break
+    end
+  end
+  puts "parsed in #{parsetime}"
+  #puts JSON.generate(nudata)
+  #puts JSON.pretty_generate(nudata)
+  #puts nudata.inspect
 end
 
 #[
