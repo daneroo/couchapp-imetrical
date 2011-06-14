67447c6e4cb148a948e952895fcc07b2ab7f4bb0
Daniel Lauzon <daniel.lauzon@gmail.com>
2011-06-14 03:41:48 -0400

    refactor raw_to canonical, get ratio

--- seed/seed.rb
+++ seed/seed.rb
@@ -18,97 +18,24 @@ db.documents({})['rows'].each do |r|
 end
 #puts "Pre-cached all(#{globalPlayerPoints.length}) player points"
 
-if false 
-  puts "now:#{Date.today} e:#{IM::EPOCH} diff:#{Date.today-IM::EPOCH}"
-  IM::EPOCH.upto(Date.today) do |d| 
-    puts "++d:#{d}"
-  end
-  Date.today.downto(IM::EPOCH) do |d| 
-    puts "--d:#{d}"
-  end
-  puts "dt:now "+DateTime::now.to_s
-end
 
 baseURI = "http://192.168.5.2/iMetrical/getJSONForDay.php";
-table="watt_minute"; # watt,watt_tensec,watt_minute,watt_hour
-grain=60
-puts sprintf("  %22s %8s %8s %8s %8s", 'date','samples','size','samples\'','size\'')
-((Date.today-10)..Date.today-1).each do |d|
-  dt=DateTime.jd(d.jd)
-  today_t = dt.to_gm_time
-  today_str=today_t.strftime(IM::FMT8601)
+table="watt_tensec"; # watt,watt_tensec,watt_minute,watt_hour
+grain=10
+puts sprintf("%22s %10s %8s %8s %5s",'date','method','samples','size','ratio')
+((Date.today-10)..Date.today-1).each do |d_d|
+  d_dt=DateTime.jd(d_d.jd)
+  d_t = d_dt.to_gm_time
+  d_str=d_t.strftime(IM::FMT8601)
   
-  url = "#{baseURI}?day=#{d}&table=#{table}"
-  r = Net::HTTP.get_response( URI.parse( url ) )
+  url = "#{baseURI}?day=#{d_d}&table=#{table}"
+  resp = Net::HTTP.get_response( URI.parse( url ) )
   
-  json = r.body
-  data = JSON.parse(json)
-  canonical=IM.raw_to_canonical(today_str,data,grain,false)
-  canonical_p=IM.raw_to_canonical_byparse(today_str,data,grain,false)
-  
-  #puts JSON.generate(canonical)  #.length
-  
-  # Speedup :
-  # convert [{w,s},{w,s}] to {w:s,w:s}
-  # (0.86400).each {|idx| fmt_and_lookup_and_remove_instead_of_parse}
-  #  check no duplicates (can't cause of mysql index on stamp)
-  #  check no unremoved key in converted hash...
-  nudata = {
-    "stamp" => today_str,
-    "grain" => grain,
-    "values" =>[],
-  }
-  
-  itertime = Benchmark.realtime do
-    stamp_to_watt = {}
-    data.each do |pair|
-      stamp_to_watt[pair["stamp"]]=pair["watt"]
-    end
-    (today_t...today_t+86400).step(grain).each do |t|
-      t_str=t.strftime"%Y-%m-%dT%H:%M:%SZ"
-      idx = (t-today_t).to_i / (grain)
-      #w = stamp_to_watt[t_str]
-      # simultaneous lookup and delete
-      w = stamp_to_watt.delete(t_str)
-      if w==nil
-        #puts "!found #{t_str}: #{w}"        
-      end  
-      nudata["values"][idx]=w
-    end
-    # check that all values were removed
-    if !stamp_to_watt.empty?
-      puts "stamp->watt has #{stamp_to_watt.length} values"
-    end
-  end
-  #puts JSON.generate(nudata)  #.length
-
-  pdata=nil
-  if true
-    nudata = {
-      "stamp" => today_str,
-      "grain" => grain,
-      "values" =>[],
-    }
-    parsetime = Benchmark.realtime do
-      data.each do |pair|
-        t=DateTime.strptime(pair["stamp"]).to_gm_time
-        idx = (t-today_t).to_i / grain
-        #pair["t"] = t.strftime"%Y-%m-%dT%H:%M:%SZ"
-        #pair["idx"]=idx
-        nudata["values"][idx]=pair["watt"]
-        #puts pair.inspect
-        #break
-      end
-    end
-    #puts "parsed in #{parsetime}s"
-    #puts JSON.generate(nudata).length
-    pdata=nudata
-  end
-  #puts JSON.pretty_generate(nudata)
-  #puts nudata.inspect
-
-  puts sprintf("1-%22s %8d %8d %8d %8d", today_str,data.length,json.length,canonical["values"].length,JSON.generate(canonical).length)
-  puts sprintf("2-%22s %8d %8d %8d %8d", today_str,data.length,json.length,nudata["values"].length,JSON.generate(nudata).length)
-  puts sprintf("3-%22s %8d %8d %8d %8d", today_str,data.length,json.length,pdata["values"].length,JSON.generate(pdata).length)
-  puts sprintf("4-%22s %8d %8d %8d %8d", today_str,data.length,json.length,canonical_p["values"].length,JSON.generate(canonical_p).length)
+  json_raw = resp.body
+  raw = JSON.parse(json_raw)
+  canonical=IM.raw_to_canonical(d_str,raw,grain,false)
+  json_canonical = JSON.generate(canonical)
+  ratio = json_raw.length*1.0/json_canonical.length
+  puts sprintf("%22s %10s %8d %8d", d_str,'raw',raw.length,json_raw.length)
+  puts sprintf("%22s %10s %8d %8d %5.2f", d_str,'canonical',canonical["values"].length,json_canonical.length,ratio)
 end
