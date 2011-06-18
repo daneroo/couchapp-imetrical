63163b05e8f198da4a8347d5dc3a40bfe827ac1e
Daniel Lauzon <daniel.lauzon@gmail.com>
2011-06-18 15:38:32 -0400

    starting entropy

--- seed/seed.js
+++ seed/seed.js
@@ -30,18 +30,44 @@ var options = {
     path: '/iMetrical/getJSONForDay.php?offset='+1+'&table='+table
 };
 
+var H = function(values){
+    histo={};
+    iM.rangeStepDo(0,values.length,1,function(i){
+        v = values[i];
+        if (histo[v]===undefined) histo[v]=0;
+        (histo[v]++);
+    });
+    var nkeys=0; 
+    var sumplogp
+    for (k in histo) { 
+        nkeys++;
+        if (histo[k]>0){
+            var p = histo[k]*1.0/values.length;
+            var plogp = p*Math.log(p);
+            sumplogp += p*Math.log(p)
+            console.log("k:%s p:%s p log p: %s sum:%s",k,p,plogp,sumplogp);
+        } else {
+            console.log("excluding k:%s histo[k]:%s",k,histo[k]);
+        }
+     }
+     sumplogp = -sumplogp/Math.LN2;
+    console.log("values has %d symbols with H(x)=%s bits/sample",nkeys,sumplogp);
+}
 var report = function(startStr,name,canonical,jsonRaw) {
   var canonicalJSON = JSON.stringify(canonical);
   var ratio = Math.round(100*jsonRaw.length/canonicalJSON.length)/100;
-  console.log(tf.sprintf("%22s %10s %8d %8d %7.2f",startStr,name,canonical.values.length,canonicalJSON.length,ratio));
+  var bps = canonicalJSON.length/86400/canonical.grain;
+  var h = H(canonical.values);
+  console.log(tf.sprintf("%22s %10s %8d %8d %7.2f %7.2f",startStr,name,canonical.values.length,canonicalJSON.length,ratio,bps));
 }
+
 
 var handleData = function(json){
     //console.log('json:'+json);
     data = JSON.parse(json);
     startStr = data[0]['stamp'];
-    console.log(tf.sprintf("%22s %10s %8s %8s %7s",'date','method','samples','size','ratio'));
-    console.log(tf.sprintf("%22s %10s %8d %8d %7.2f",startStr,'raw',data.length,json.length,1.0));
+    console.log(tf.sprintf("%22s %10s %8s %8s %7s %7s %7s",'date','method','samples','size','ratio','Bps','H(x)'));
+    console.log(tf.sprintf("%22s %10s %8d %8d %7.2f %7.2f",startStr,'raw',data.length,json.length,1.0,json.length/86400/grain));
     var values = iM.rawToCanonical(json,startStr,grain,false);
     var canonical = {
         "stamp" : startStr,
